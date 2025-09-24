319bc2df400a50e65aa77fbcb4eb569998c9c346
tpate0562 <56415907+tpate0562@users.noreply.github.com>
2025-09-24 14:34:51 -0700

    Initial Commit

---
+++ NextBusTests/NextBusTests.swift
@@ -0,0 +1,17 @@
+//
+//  NextBusTests.swift
+//  NextBusTests
+//
+//  Created by Tejas Patel on 9/24/25.
+//
+
+import Testing
+@testable import NextBus
+
+struct NextBusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ NextBusUITests/NextBusUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  NextBusUITestsLaunchTests.swift
+//  NextBusUITests
+//
+//  Created by Tejas Patel on 9/24/25.
+//
+
+import XCTest
+
+final class NextBusUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
